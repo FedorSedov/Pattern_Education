c324d54b80fd1f462dd7a2727424ae73f0c0a47c
AutoTest <fedssedov@gmail.com>
2021-07-21 00:18:05 +0300

    Initial commit Factory pattern

---
+++ ShapeCreator.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from PyQt5.QtWidgets import QGraphicsEllipseItem, QGraphicsRectItem
+from PyQt5.QtGui import QPen, QColor
+from main import Ui_MainWindow
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def get_shape(self):
+        """Shape interface"""
+        pass
+
+
+class CircleShape(Shape):
+    def __init__(self):
+        self.item = QGraphicsEllipseItem(20, 20, 250, 250)
+        self.item.setPen(QPen(QColor("black")))
+
+    def get_shape(self):
+        return self.item
+
+
+class SquareShape(Shape):
+    def __init__(self):
+        self.item = QGraphicsRectItem(20, 20, 250, 250)
+        self.item.setPen(QPen(QColor("black")))
+
+    def get_shape(self):
+        return self.item
+
+
+class ShapeCreator(ABC):
+
+    @abstractmethod
+    def factory_method(self):
+        pass
+
+    def draw(self, ui):
+        item = self.factory_method()
+        ui.draw_ui(item.get_shape())
+        '''Ui_MainWindow.draw_on_scene(ui, item.get_shape())'''
+
+
+class CircleCreator(ShapeCreator):
+    def factory_method(self):
+        return CircleShape()
+
+
+class SquareCreator(ShapeCreator):
+    def factory_method(self):
+        return SquareShape()
+
+
+def Creator_Execute(ShapeCreator, ui):
+    ShapeCreator.draw(ui)
+
